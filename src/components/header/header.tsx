68084a6d1a7687a7f8c56dc9fa83ee14b11f832d
Nikolay <napadaylonikolay@gmail.com>
2022-08-07 13:52:27 +0600

    first commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { useState } from "react"
+import { OneDiv } from "./header.styled"
+
+
+const Header = () => {
+    // const [asd, setAsd] = useState<any>()
+    return (
+        <div>
+            <OneDiv>
+                HEADER
+            </OneDiv>
+        </div>
+    )
+}
+
+export default Header;
