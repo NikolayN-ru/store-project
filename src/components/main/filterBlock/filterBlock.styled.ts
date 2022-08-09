74242b228bd3e239a154abf042728261fee89401
Андрей Черников <ac@x-lab.com.ru>
2022-08-09 20:53:28 +0500

    Стилизиация чекбоксов

--- src/components/main/filterBlock/filterBlock.styled.ts
+++ src/components/main/filterBlock/filterBlock.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const FilterBlockWrapper = styled('div')`
-& {
+  & {
     width: 300px;
     display: flex;
     flex-direction: column;
@@ -11,44 +11,53 @@ export const FilterBlockWrapper = styled('div')`
     align-self: flex-start;
     padding-top: 50px;
 
-}
+  }
 `
 
 export const HeaderBlock = styled('h2')`
-& {
+  & {
     color: #555555;
     font-size: 18px;
     margin-top: 20px;
     margin-bottom: 10px;
-}
+  }
 `
 
 export const FilterCoast = styled('div')`
-& {
+  & {
     width: 100%;
     padding-bottom: 20px;
     border-bottom: 1px solid #BDBDBD;
-}
+  }
 `
 
 export const Input = styled('input')`
-& {
+  & {
     type: range;
-}
+  }
 `
 
 export const FilterCompound = styled('div')`
-& {
+  & {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     padding-bottom: 20px;
     border-bottom: 1px solid #BDBDBD;
-}
+  }
 `
 
 export const InputCheckbox = styled('input')`
-& {
+  & {
+    width: 14px;
+    height: 14px;
+    border: 1px solid black;
+		border-radius: 4px;
+    outline: none;
     /* type: checkbox; */
-}
+  }
+
+  &:active {
+    background-color: cornflowerblue;
+  }
 `
--- src/components/main/filterBlock/filterBlock.tsx
+++ src/components/main/filterBlock/filterBlock.tsx
@@ -1,45 +1,51 @@
-import React, { FC } from "react"
-import { FilterBlockWrapper, FilterCoast, FilterCompound, HeaderBlock, Input, InputCheckbox } from "./filterBlock.styled";
+import React, {FC} from "react"
+import {FilterBlockWrapper, FilterCoast, FilterCompound, HeaderBlock, Input, InputCheckbox} from "./filterBlock.styled";
 
 const state = ["меринос", "кашемир", "ангора", "лама", "вискоза", "мохер", "як", "люрекс"]
 const stateMetr = ["50-100", "100-150", "150-200", "250-300", "свыше 300"]
 
 const FilterBlock: FC = () => {
-    return (
-        <FilterBlockWrapper>
-            <HeaderBlock>
-                фильтр по пряже:
-            </HeaderBlock>
-            <FilterCoast>
-                <HeaderBlock>
-                    Стоимость:
-                </HeaderBlock>
-                <Input />
-            </FilterCoast>
-
-            <FilterCoast>
-                <HeaderBlock>
-                    Метраж:
-                </HeaderBlock>
-                {stateMetr.map((item, id) => {
-                    return (
-                        <InputCheckbox key={id} placeholder={item} />
-                    )
-                })}
-            </FilterCoast>
-
-            <FilterCompound>
-                <HeaderBlock>
-                    По составу:
-                </HeaderBlock>
-                {state.map((item, id) => {
-                    return (
-                        <InputCheckbox key={id} placeholder={item} />
-                    )
-                })}
-            </FilterCompound>
-        </FilterBlockWrapper>
-    )
+	return (
+		<FilterBlockWrapper>
+			<HeaderBlock>
+				фильтр по пряже:
+			</HeaderBlock>
+			<FilterCoast>
+				<HeaderBlock>
+					Стоимость:
+				</HeaderBlock>
+				<Input/>
+			</FilterCoast>
+			
+			<FilterCoast>
+				<HeaderBlock>
+					Метраж:
+				</HeaderBlock>
+				{stateMetr.map((item, id) => {
+					return (
+						<label style={{display: 'block', fontSize: 15, color: 'rgba(70,70,70,.9)'}}>
+							<InputCheckbox key={id} placeholder={item} type={'checkbox'} checked={true}/>
+							{item}
+						</label>
+					)
+				})}
+			</FilterCoast>
+			
+			<FilterCompound>
+				<HeaderBlock>
+					По составу:
+				</HeaderBlock>
+				{state.map((item, id) => {
+					return (
+						<label style={{display: 'block', fontSize: 15, color: 'rgba(70,70,70,.9)'}}>
+							<InputCheckbox key={id} placeholder={item} type={'checkbox'} checked={true}/>
+							{item}
+						</label>
+					)
+				})}
+			</FilterCompound>
+		</FilterBlockWrapper>
+	)
 }
 
 export default FilterBlock;
