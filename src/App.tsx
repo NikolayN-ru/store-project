1b0a52e804827c3b9c866f2f3a88719a71af9f5d
Nikolay <napadaylonikolay@gmail.com>
2022-08-19 22:50:37 +0600

    relog-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { createContext, FC } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from './components/WrapperComponents/Footer/Footer';
@@ -13,32 +13,49 @@ import Person from './pages/Person/Person';
 import ExtraHeader from './components/ExtraHeader/ExtraHeader';
 import Cart from './pages/Cart/Cart';
 import Order from './pages/Order/Order';
+import Store from './store/store';
 
-const About = React.lazy(()=> import('./pages/About/About'));
+interface IStore {
+  store: Store,
+}
+
+const store = new Store();
+
+export const Context = createContext<IStore>({
+  store,
+});
+
+
+const About = React.lazy(() => import('./pages/About/About'));
+
 
 const App: FC = () => {
   return (
-    <Router>
-      <ExtraHeader/>
-      <Header />
-      <Routes>
+    <Context.Provider value={{
+      store
+    }}>
+      <Router>
+        <ExtraHeader />
+        <Header />
+        <Routes>
           <Route path="/about" element={
             <React.Suspense fallback='loading'>
-          <About />
+              <About />
             </React.Suspense>
           } />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/yarn" element={<Main />} />
-        <Route path="/needles" element={<Needles />} />
-        <Route path="/product" element={<Wares />} />
-        <Route path="/" element={<Main />} />
-        <Route path="/item/:itemId" element={<Product />} />
-        <Route path="/login" element={<Person />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<Order />} />
-      </Routes>
-      <Footer />
-    </Router>
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/yarn" element={<Main />} />
+          <Route path="/needles" element={<Needles />} />
+          <Route path="/product" element={<Wares />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/item/:itemId" element={<Product />} />
+          <Route path="/login" element={<Person />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Context.Provider>
   );
 }
 
--- src/components/PopupLogin/PopupLogin.tsx
+++ src/components/PopupLogin/PopupLogin.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from "react"
+import React, { FC, useContext, useState } from "react"
+import { Context } from "../../App";
 import { ButtonClose, Input, PopupWrapper } from "./PopupLogin.styled";
 
 const PopupLogin: FC = () => {
-    // const [stateLike, setStateLike] = useState(false);
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const {store} = useContext(Context);
+
     return (
         <PopupWrapper>
-            <ButtonClose/>
-            Login
-            <Input/>
-            <Input/>
-            <button>+</button>
+            <ButtonClose />
+            Login+
+            <Input
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+                placeholder="email"
+            />
+            <Input
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                placeholder="password"
+            />
+            <button onClick={() => store.login(email, password)}>__LOGIN__</button>
+            <button onClick={() => store.registration(email, password)}>REGISTRATION</button>
         </PopupWrapper>
     )
 }
